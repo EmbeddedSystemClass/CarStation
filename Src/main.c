136765be5c4ac1f7ae7bd3874c2366612c1b95df
danielchen76 <jianqing.chan@gmail.com>
2014-06-25 20:39:23 +0800

    ugfx ok. make -r (fast)

--- Src/main.c
+++ Src/main.c
@@ -42,6 +42,8 @@ static msg_t Thread1(void *arg) {
  */
 int main(void) {
 	font_t		font;
+	GHandle GW1, GW2, GW3;
+	int			i;
 
   /*
    * System initializations.
@@ -74,12 +76,50 @@ int main(void) {
   //gdispDrawLine(50, 50, 50, 0, 1);
   //gdispFlush();
 
-  font = gdispOpenFont("UI2");
-  gdispDrawString(10, 10, "This is test.", font, 1);
-
-  //gdispDrawLine(0, 0, 127, 63, 1);
+  font = gdispOpenFont("fixed_5x8");
+  gdispDrawString(0, 0, "This is test.", font, 1);
+  gdispCloseFont(font);
   gdispFlush();
 
+  font = gdispOpenFont("LargeNumbers");
+  gdispDrawString(0, 10, "12:30", font, 1);
+  gdispCloseFont(font);
+  gdispFlush();
+
+  gdispSetPowerMode(powerOff);
+  gdispSetPowerMode(powerOn);
+
+  gdispSetContrast(50);
+  gdispSetContrast(0);
+  gdispSetContrast(100);
+  gdispSetContrast(50);
+
+  chThdSleepMilliseconds(3000);
+
+  // test consle
+  font = gdispOpenFont("UI2");
+
+  {
+	  GWindowInit		wi;
+
+	  wi.show = TRUE;
+	  wi.x = 0; wi.y = 0; wi.width = gdispGetWidth(); wi.height = gdispGetHeight();
+	  GW1 = gwinConsoleCreate(0, &wi);
+  }
+
+  gwinSetFont(GW1, font);
+  gwinSetColor(GW1, 1);
+  gwinClear(GW1);
+  gdispFlush();
+
+  // write console
+  i = 0;
+  while (TRUE)
+  {
+	  gwinPrintf(GW1,"Test \033bnumber \033B%d.\r\n", i++);
+	  gdispFlush();
+	  gfxSleepMilliseconds(1000);
+  }
 
 	while(TRUE) {
 		chThdSleepMilliseconds(250);
